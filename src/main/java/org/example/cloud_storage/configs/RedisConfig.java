0d45a307fea1c2a6f47930e1c51cc64ccbb3dbc3
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 17:35:22 +0300

    feat: approach connectionFactory using CacheManager replaced by LettuceConnectionFactory

--- src/main/java/org/example/cloud_storage/configs/RedisConfig.java
+++ src/main/java/org/example/cloud_storage/configs/RedisConfig.java
@@ -1,38 +1,27 @@
 package org.example.cloud_storage.configs;
 
-import org.springframework.cache.CacheManager;
+import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 
 @Configuration
+@AllArgsConstructor
 @EnableCaching
+@EnableRedisIndexedHttpSession
 public class RedisConfig {
 
     @Bean
-    public CacheManager cacheManager(
-            RedisConnectionFactory redisConnectionFactory
-    ) {
-        RedisCacheConfiguration config = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .serializeKeysWith(
-                        RedisSerializationContext
-                                .SerializationPair
-                                .fromSerializer(new StringRedisSerializer())
-                )
-                .serializeValuesWith(
-                        RedisSerializationContext
-                                .SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
-                );
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(config)
-                .build();
+    @Primary
+    public LettuceConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration("localhost", 6379);
+        configuration.setPassword("");
+        System.out.println("LettuceConnectionFactory initialized");
+        return new LettuceConnectionFactory(configuration);
     }
 }
