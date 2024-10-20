8ac9198b43f948bd0a0f2c2b52f52e57c22b1cb4
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 17:38:09 +0300

    fix: Added beans: HttpSessionIdResolver, HttpSessionEventPublisher for correct redis work.

--- src/main/java/org/example/cloud_storage/configs/SessionConfig.java
+++ src/main/java/org/example/cloud_storage/configs/SessionConfig.java
@@ -2,23 +2,31 @@ package org.example.cloud_storage.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
-@EnableSpringHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory);
-        return template;
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return new CookieHttpSessionIdResolver();
+    }
+
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
     }
 }
