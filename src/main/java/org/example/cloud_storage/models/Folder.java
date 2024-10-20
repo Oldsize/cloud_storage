3eb3f19dd07eeab9785370f42162d4d57218efe9
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 17:15:09 +0300

    fix: Deleted @Data lombok annotation. @EqualsAndHashCode annotation replaced by hand-created methods

--- src/main/java/org/example/cloud_storage/models/Folder.java
+++ src/main/java/org/example/cloud_storage/models/Folder.java
@@ -1,13 +1,23 @@
 package org.example.cloud_storage.models;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.cache.annotation.Cacheable;
+
+import java.util.Objects;
+
 
 @Entity
 @Cacheable("folders")
 @Table(schema = "public", name = "folders")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Folder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +36,19 @@ public class Folder {
         this.user = user;
     }
 
-    public Folder() {
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Folder folder = (Folder) o;
+        return getId() != null && Objects.equals(getId(), folder.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
--- src/main/java/org/example/cloud_storage/models/User.java
+++ src/main/java/org/example/cloud_storage/models/User.java
@@ -2,11 +2,17 @@ package org.example.cloud_storage.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 
 @Entity
 @Cacheable
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Table(schema = "public", name = "users")
 public class User {
     @Id
@@ -17,7 +23,7 @@ public class User {
     @Column(name = "username", unique = true, nullable = false, updatable = false)
     private String username;
 
-    @Column(name = "password", unique = true, nullable = false, updatable = false)
+    @Column(name = "password", nullable = false, updatable = false)
     private String password;
 
     public User(String username, String password) {
@@ -25,6 +31,21 @@ public class User {
         this.password = password;
     }
 
-    public User() {
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
