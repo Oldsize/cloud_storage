1f1d3147d1544cc7d9a8527c879539ee9e83f572
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 17:40:13 +0300

    fix: Added bean SpringSessionBackedSessionRegistry sessionRegistry, AuthenticationManager. fix: rewritten method securityFilterChain for more detailed settings.

--- src/main/java/org/example/cloud_storage/configs/SecurityConfig.java
+++ src/main/java/org/example/cloud_storage/configs/SecurityConfig.java
@@ -1,30 +1,41 @@
 package org.example.cloud_storage.configs;
 
-import org.example.cloud_storage.models.User;
 import org.example.cloud_storage.repositories.UserRepository;
 import org.example.cloud_storage.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.Session;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserRepository userRepository;
+    RedisIndexedSessionRepository redisIndexedSessionRepository;
 
     @Autowired
-    public SecurityConfig(UserRepository userRepository) {
+    public SecurityConfig(UserRepository userRepository, RedisIndexedSessionRepository redisIndexedSessionRepository) {
         this.userRepository = userRepository;
+        this.redisIndexedSessionRepository = redisIndexedSessionRepository;
     }
 
     @Bean
@@ -32,34 +43,63 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/register", "/login", "/public/**").permitAll()
+                        .requestMatchers("/register", "/login", "/logout", "/public/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::newSession)
+                        .maximumSessions(1)
+                        .sessionRegistry(sessionRegistry())
+                        .maxSessionsPreventsLogin(false)
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .permitAll()
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/home")
+                        .failureUrl("/login?error")
                 )
-                .logout(LogoutConfigurer::permitAll);
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("SESSION")
+                        .logoutSuccessUrl("/login")
+                );
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> {
-            User user = userRepository.findByUsername(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-            return new CustomUserDetails(user);
-        };
+        return username -> userRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return new ProviderManager(provider);
+    }
+
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
+    public void configure(AuthenticationManagerBuilder auth) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        auth.authenticationProvider(provider);
+    }
+
+    @Bean
+    public SpringSessionBackedSessionRegistry<? extends Session> sessionRegistry() {
+        return new SpringSessionBackedSessionRegistry<>(this.redisIndexedSessionRepository);
     }
 }
