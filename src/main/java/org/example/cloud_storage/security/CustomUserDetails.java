6c04478bd6cc201933863c6b8b2e64f8639e997d
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 16:58:31 +0300

    fix: userRole parameter for super: null replaced by Collections.emptyList()

--- src/main/java/org/example/cloud_storage/security/CustomUserDetails.java
+++ src/main/java/org/example/cloud_storage/security/CustomUserDetails.java
@@ -3,12 +3,15 @@ package org.example.cloud_storage.security;
 import lombok.Getter;
 import org.example.cloud_storage.models.User;
 
+import java.util.Collections;
+
+
 @Getter
 public class CustomUserDetails extends org.springframework.security.core.userdetails.User {
     private final Long id;
 
     public CustomUserDetails(User user) {
-        super(user.getUsername(), user.getPassword(), null);
+        super(user.getUsername(), user.getPassword(), Collections.emptyList());
         this.id = user.getId();
     }
 }
