c215a2d9beab63d6b42fc60ae8795fc7656230fc
oldsize <gytkivgu200gyt@gmail.com>
2024-10-20 17:16:06 +0300

    fix: The approach of getting the Principal object from method parameters has been replaced by getting the object from the SecurityContextHolder. feat: Added hand @PostMapping("rename")

--- src/main/java/org/example/cloud_storage/controllers/FolderController.java
+++ src/main/java/org/example/cloud_storage/controllers/FolderController.java
@@ -1,22 +1,26 @@
 package org.example.cloud_storage.controllers;
 
+import org.example.cloud_storage.dto.RenameFolderRequest;
 import org.example.cloud_storage.security.CustomUserDetails;
 import org.example.cloud_storage.services.FolderService;
 import org.example.cloud_storage.services.MinioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 
-@RestController
+@Controller
 @RequestMapping("/api/folder")
 public class FolderController {
 
-    FolderService folderService;
-    MinioService minioService;
+    private final FolderService folderService;
+    private final MinioService minioService;
 
     @Autowired
     public FolderController(FolderService folderService, MinioService minioService) {
@@ -25,27 +29,36 @@ public class FolderController {
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam String name,
-                         Principal principal) {
+    public String create(@RequestParam String name) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof CustomUserDetails) {
             Long userid = ((CustomUserDetails) principal).getId();
             folderService.add(name, userid);
         }
-        return "foldersHome";
+        return "redirect:home";
     }
 
     @PostMapping("/remove")
-    public String remove(@RequestParam String name,
-                         Principal principal) {
+    public String remove(@RequestParam String name) throws Exception {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof CustomUserDetails) {
             Long userid = ((CustomUserDetails) principal).getId();
+            minioService.removeAllByFolder("user-files", name, userid);
             folderService.remove(name, userid);
-            try {
-                minioService.removeAllByFolder("user-files", name, userid);
-            } catch (Exception e) {
-                // todo redirect на страницу с ошибкой
-            }
         }
-        return "foldersHome";
+        return "redirect:home";
+    }
+
+    @PostMapping("/rename")
+    public ResponseEntity<String> rename(@RequestBody RenameFolderRequest request) throws Exception {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String oldName = request.getOldName();
+        String newName = request.getNewName();
+        if (principal instanceof CustomUserDetails) {
+            Long userid = ((CustomUserDetails) principal).getId();
+            minioService.renameFolder("user-files", oldName, userid, newName);
+            return ResponseEntity.ok("Папка успешно переименована");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Пользователь не авторизован");
     }
 }
